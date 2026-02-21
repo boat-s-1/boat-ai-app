884d1d92d1e4d3dbff792fad175be3af56e1b3e9
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-21 10:53:08 +0900

    Update public_app.py

--- public_app.py
+++ public_app.py
@@ -211,7 +211,7 @@ input_rows.append({
             "回り足": mawari
         })
 
-    input_df = pd.DataFrame(input_rows).set_index("艇番")
+input_df = pd.DataFrame(input_rows).set_index("艇番")
 
     st.divider()
     st.markdown("### 公式展示タイム表")
@@ -436,6 +436,7 @@ with tab5:
         st.markdown(html, unsafe_allow_html=True)
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
