b6c2e1641524b484a9906d9fb36a9572c94503dc
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-26 01:06:48 +0900

    Update public_app.py

--- public_app.py
+++ public_app.py
@@ -10,7 +10,8 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     if st.button("蒲郡", use_container_width=True):
-        st.switch_page("pages/07_蒲郡.py")
+       st.switch_page("pages/07_蒲郡.py")
+
 
 
 
