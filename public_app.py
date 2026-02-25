ab20e5a01f934af8a5c0ddddc4c2621030eb5f7c
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-26 04:13:09 +0900

    Update public_app.py

--- public_app.py
+++ public_app.py
@@ -6,10 +6,17 @@ import os
 st.set_page_config(page_title="競艇Pro", layout="wide")
 st.title("🏁 会場を選択")
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3,col4,col5 = st.columns(3)
 
 with col1:
-    if st.button("蒲郡", use_container_width=True):
+    if st.button("桐生", use_container_width=True):
+        # 候補となるパスをすべて試す
+        targets = [
+            "pages/07_gamagori.py",
+            "pages/pages/07_gamagori.py",
+            "07_gamagori.py"
+with col1:
+     if st.button("蒲郡", use_container_width=True):
         # 候補となるパスをすべて試す
         targets = [
             "pages/07_gamagori.py",
@@ -26,4 +33,5 @@ with col1:
         
         if not found:
             st.error("ファイルが見つかりません。")
+
 
