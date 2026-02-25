0e0f1283e53a94f690cd50e0686c22366952a239
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-26 04:16:27 +0900

    Update public_app.py

--- public_app.py
+++ public_app.py
@@ -1,27 +1,25 @@
 import streamlit as st  # これが必要です！
 
 # --- 以前お渡しした「自動検索コード」を使う場合 ---
-import os
+import streamlit as st
 
 st.set_page_config(page_title="競艇Pro", layout="wide")
+
 st.title("🏁 会場を選択")
 
-col1, col2, col3,col4,col5 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    if st.button("桐生", use_container_width=True):
-        # 候補となるパスをすべて試す
-        targets = [
-            "pages/07_gamagori.py",
-            "pages/pages/07_gamagori.py",
-            "07_gamagori.py"
-with col1:
-     if st.button("蒲郡", use_container_width=True):
-        # 候補となるパスをすべて試す
-        targets = [
-            "pages/07_gamagori.py",
-            "pages/pages/07_gamagori.py",
-            "07_gamagori.py"
+    if st.button("蒲郡", use_container_width=True):
+        st.switch_page("pages/07_gamagori.py")
+
+with col2:
+    if st.button("大村", use_container_width=True):
+        st.switch_page("pages/01_omura.py")
+
+with col3:
+    if st.button("常滑", use_container_width=True):
+        st.switch_page("pages/02_tokoname.py")
         ]
         
         found = False
@@ -33,5 +31,6 @@ with col1:
         
         if not found:
             st.error("ファイルが見つかりません。")
+
 
 
