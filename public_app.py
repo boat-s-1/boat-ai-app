2f930b5489f05b1a81b186363e9493cfbcefe15d
boat-s-1 <marron.toyohashi@gmail.com>
2026-02-26 09:26:04 +0900

    Update public_app.py

--- public_app.py
+++ public_app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="競艇Pro", layout="wide")
 
 st.title("🏁 会場を選択")
 
-col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, = st.columns(7)
+col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, = st.columns(24)
 
 with col1:
     if st.button("桐生01", use_container_width=True):
@@ -101,6 +101,7 @@ with col23:
 with col24:
     if st.button("大村", use_container_width=True):
         st.switch_page("pages/24_omura.py")
+
 
 
 
